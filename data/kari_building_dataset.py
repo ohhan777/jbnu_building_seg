1cc8a5bf072829bc3806934c9580e903bde22572
Han Oh <ohhan@kari.re.kr>
2023-08-10 21:27:12 +0900

    dataset

--- data/kari_building_dataset.py
+++ data/kari_building_dataset.py
@@ -1,42 +1,36 @@
 import torch
 from pathlib import Path
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 import numpy as np
 import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class KariBuildingDataset(torch.utils.data.Dataset):
-    def __init__(self, root, is_train=False):
+    def __init__(self, root, train=False):
         self.root = Path(root)
-        self.is_train = is_train
-        if is_train:
+        self.train = train
+        if train:
             self.img_dir = self.root/'kari_building_v1.5/train/images'
         else:
             self.img_dir = self.root/'kari_building_v1.5/val/images'
-        self.img_filenames = sorted(self.img_dir.glob('*.png'))
-        self.transform = get_transforms(is_train)
+        self.img_files = sorted(self.img_dir.glob('*.png'))
+        self.transform = get_transforms(train)
 
     def __getitem__(self, idx):
-        img_filename = self.img_filenames[idx]
-        label_filename = img_filename.as_posix().replace('images/', 'png_labels/').replace('.png', '_label.png')
-        img = cv2.imread(img_filename.as_posix())
-        label_img = cv2.imread(label_filename, cv2.IMREAD_GRAYSCALE)
+        img_file= self.img_files[idx]
+        label_file = img_file.as_posix().replace('images/', 'png_labels/').replace('.png', '_label.png')
+        img = cv2.imread(img_file.as_posix())
+        label_img = cv2.imread(label_file, cv2.IMREAD_GRAYSCALE)
         img, label_img = self.transform(img, label_img)
         return img, label_img
 
-
     def __len__(self):
-        return len(self.img_filenames)
+        return len(self.img_files)
     
-    
-
-
-
-
 class ImageAug:
-    def __init__(self, is_train):
-        if is_train:
-            self.aug = A.Compose([A.RandomCrop(512, 512),
+    def __init__(self, train):
+        if train:
+            self.aug = A.Compose([A.RandomCrop(256, 256),
                                   A.HorizontalFlip(p=0.5),
                                   A.ShiftScaleRotate(p=0.3),
                                   A.RandomBrightnessContrast(p=0.3),
